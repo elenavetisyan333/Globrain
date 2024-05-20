3a2a3de9a0659de9ae85a8440497a75e40feed77
elenavetisyan333 <elen.avetisyan.333@mail.ru>
2024-05-20 21:14:08 +0400

    handle user Signing out with Firebase

--- app/src/main/java/com/example/globrain/ProfileFragment.java
+++ app/src/main/java/com/example/globrain/ProfileFragment.java
@@ -1,18 +1,38 @@
 package com.example.globrain;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
 
 import androidx.fragment.app.Fragment;
 
-import com.example.globrain.R;
+import com.google.firebase.auth.FirebaseAuth;
 
 public class ProfileFragment extends Fragment {
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
-        return inflater.inflate(R.layout.fragment_profile, container, false);
+        View view = inflater.inflate(R.layout.fragment_profile, container, false);
+
+        Button signOutBtn = view.findViewById(R.id.signOutBtn);
+
+        signOutBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                logout();
+            }
+        });
+
+        return view;
+    }
+
+    private void logout() {
+        FirebaseAuth.getInstance().signOut();
+        Intent intent = new Intent(getActivity(), SignInActivity.class);
+        startActivity(intent);
+        getActivity().finish();
     }
 }
