92f009c82a598976615aa755b863f58feaadb07d
elenavetisyan333 <elen.avetisyan.333@mail.ru>
2024-05-27 02:28:08 +0400

    show email in profile fragment

--- app/src/main/java/com/example/globrain/ProfileFragment.java
+++ app/src/main/java/com/example/globrain/ProfileFragment.java
@@ -1,23 +1,35 @@
 package com.example.globrain;
 
+import android.annotation.SuppressLint;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
+import android.widget.TextView;
 
 import androidx.fragment.app.Fragment;
 
 import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
 
 public class ProfileFragment extends Fragment {
 
+    private TextView emailTextView;
+    private Button signOutBtn;
+
+    private FirebaseAuth mAuth;
+
+    @SuppressLint("MissingInflatedId")
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
         View view = inflater.inflate(R.layout.fragment_profile, container, false);
 
-        Button signOutBtn = view.findViewById(R.id.signOutBtn);
+        mAuth = FirebaseAuth.getInstance();
+
+        emailTextView = view.findViewById(R.id.emailTextView);
+        signOutBtn = view.findViewById(R.id.signOutBtn);
 
         signOutBtn.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -25,6 +37,12 @@ public class ProfileFragment extends Fragment {
                 logout();
             }
         });
+
+        FirebaseUser currentUser = mAuth.getCurrentUser();
+        if (currentUser != null) {
+            String email = currentUser.getEmail();
+            emailTextView.setText(email);
+        }
 
         return view;
     }
